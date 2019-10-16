742ef2b6c6c51752220522ada61b04ac663c5da5
wesley-ruede <wesley.k.r1993@gmail.com>
2019-10-16 19:00:06 -0400

    a calculator I built a while ago

---
+++ Calculator/Calc_out.py
@@ -0,0 +1,2 @@
+from Calculator import Calculator
+
---
+++ Calculator/Calculator.py
@@ -0,0 +1,42 @@
+class Calculator:
+    def __init__(self):
+        self.x = 0
+        self.x = 0
+
+    def add(self):
+        x = int(input('Enter an integer: '))
+        y = int(input('Enter an integer: '))
+        return(x + y)
+
+    def subtract(self):
+        x = input('Enter an integer: ')
+        y = input('Enter an integer: ')
+        return(x - y)
+
+    def multiply(self):
+        x = input('Enter an integer: ')
+        y = input('Enter an integer: ')
+        return(x * y)
+
+    def divide(self):
+        x = int(input('Enter an integer: '))
+        y = int(input('Enter an integer: '))
+        return(x / y)
+
+    def modulo(self):
+        x = input('Enter an integer: ')
+        y = input('Enter an integer: ')
+        return(x % y)
+
+    def floor(self):
+        x = input('Enter an integer: ')
+        y = input('Enter an integer: ')
+        return(x // y)
+    
+calc = Calculator()
+
+def main():
+    print(calc.add())
+
+if __name__ == '__main__':
+    main()
